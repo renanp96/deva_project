e157f38c196963d285a15c3b94728b0739616261
renanp96 <renanpa1996@gmail.com>
2025-09-09 11:53:21 -0300

    Another Brick in The Wall

---
+++ src/main/java/com/api/deva/DevaApplication.java
@@ -0,0 +1,13 @@
+package com.api.deva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/deva/DevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.deva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
